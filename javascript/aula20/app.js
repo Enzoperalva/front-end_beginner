26b1a9efd3ce9a50f2c1c4ee6917af39800011ee
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-11 18:29:53 -0300

    Atividades lógicas de programação

--- javascript/aula20/app.js
+++ javascript/aula20/app.js
@@ -1,3 +1,21 @@
-const multiply = (x, y) => x * y;
+function fizzBuzz(){
+    let inicio = 1
+    let fim = 30
 
-console.log(multiply(12, 3))
+    while (inicio <= fim){
+        if (inicio % 3 === 0 && inicio % 5 === 0){
+            console.log('Fizzbuzz');
+        } else if (inicio % 3 === 0){
+            console.log('Fizz');
+        } else if (inicio % 5 === 0 ) {
+            console.log('Buzz');
+        } else {
+            console.log(inicio);
+        };
+
+        inicio++;
+    };
+};
+
+
+fizzBuzz();
