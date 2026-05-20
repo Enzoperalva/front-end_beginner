db27d877169fef3a77a8fd9c47e3221de5a40928
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-20 16:33:07 -0300

    Atividade imc site

---
+++ javascript/model/assets/js/index.js
@@ -0,0 +1,50 @@
+let pesoInput = document.getElementById('input-peso');
+let alturaInput = document.getElementById('input-altura');
+let formInput = document.getElementById('form');
+
+function recebeEventoForm(event){
+    event.preventDefault()
+    
+    let pesoValue = pesoInput.value;
+    let alturaValue = alturaInput.value;
+    let containerImc = document.getElementById('imc');
+    let calc = false;
+
+    console.log('CHEGOU AQUI')
+    if (!pesoValue){
+        containerImc.innerHTML = 'Peso inválido';
+        containerImc.style.backgroundColor = "pink"
+    } else if(!alturaValue){
+        containerImc.innerHTML = 'Altura inválida';
+        containerImc.style.backgroundColor = "pink"
+    } else{
+        calc = true
+    }
+    if (calc){
+
+        const imc = pesoValue / (alturaValue * alturaValue);
+        
+        if(imc < 18.5) {
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Abaixo do peso)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        } else if( imc <= 24.9){
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Peso normal)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        } else if(imc <= 29.9){
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Sobrepeso)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        } else if(imc <= 34.9){
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Obesidade grau 1)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        } else if(imc <= 39.9) {
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Obesidade grau 2)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        } else{
+            containerImc.innerHTML = `Seu IMC é ${imc.toFixed(2)}(Obesidade grau 3)`;
+            containerImc.style.backgroundColor = "aquamarine"
+        };
+    };
+};
+
+
+formInput.addEventListener('submit', recebeEventoForm)
